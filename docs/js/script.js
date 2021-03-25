bac2446baab7cbed55abec3d519e11a53de1d918
yuya-yamaguchi <nsy1023n@gmail.com>
2021-03-25 23:54:44 +0900

    UI等調整

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
   const kyoonMoreBtn = document.getElementById('kyoon-more-btn')
+  const tdnMoreBtn = document.getElementById('tdn-more-btn')
+  const tgMoreBtn = document.getElementById('tg-more-btn')
+  // 京音サイト詳細ボタンクリック
   kyoonMoreBtn.addEventListener('click', function() {
     const target = document.getElementById('product-info--hide');
     if (target.style.display == 'block') {
@@ -9,6 +12,30 @@ document.addEventListener("DOMContentLoaded", function(){
     else {
       target.style.display = 'block';
       kyoonMoreBtn.textContent = '▲ 詳細を閉じる'
+    };
+  })
+  // TDN詳細ボタンクリック
+  tdnMoreBtn.addEventListener('click', function() {
+    const target = document.getElementById('tdn-info-hide');
+    if (target.style.display == 'block') {
+      target.style.display = 'none';
+      tdnMoreBtn.textContent = '▼ 詳細を表示'
+    }
+    else {
+      target.style.display = 'block';
+      tdnMoreBtn.textContent = '▲ 詳細を閉じる'
+    };
+  })
+  // TG詳細ボタンクリック
+  tgMoreBtn.addEventListener('click', function() {
+    const target = document.getElementById('tg-info-hide');
+    if (target.style.display == 'block') {
+      target.style.display = 'none';
+      tgMoreBtn.textContent = '▼ 詳細を表示'
+    }
+    else {
+      target.style.display = 'block';
+      tgMoreBtn.textContent = '▲ 詳細を閉じる'
     };
   })
 }, false);
